d3770063ce10f0b1913f6f5c1301136c175e05b9
Charlanth <148385217+Charlanth@users.noreply.github.com>
2023-10-20 14:06:01 -0600

    Started to add the description of projects

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 function displayMenu() {
-    var x = document.getElementById("navbar");
+  var x = document.getElementById("navbar");
   if (x.className === "navbar") {
     x.className += " responsive";
   } else {
     x.className = "navbar";
+  }
+}
+
+function displayDescription(hiddenElementID, elementID) {
+  var element = document.getElementById(hiddenElementID);
+  var elementToHide = document.getElementById(elementID);
+  if(element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+    elementToHide.classList.add("hidden");
+  } else {
+    element.classList.add("hidden");
+    elementToHide.classList.remove("hidden");
   }
 }
