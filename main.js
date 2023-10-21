005726d0df0735bda9b8a5e25a1d47f5a68125fe
Charlanth <148385217+Charlanth@users.noreply.github.com>
2023-10-21 08:46:31 -0600

    Added translations
    Translations for French and English started.

--- main.js
+++ main.js
@@ -18,3 +18,55 @@ function displayDescription(hiddenElementID, elementID) {
     elementToHide.classList.remove("hidden");
   }
 }
+
+function showLanguageOptions() {
+  var languageOptions = document.getElementById('language-options');
+  if(languageOptions.classList.contains("hidden")) {
+    languageOptions.classList.remove("hidden");
+  } else {
+    languageOptions.classList.add("hidden");
+  }
+}
+
+var translations = {}; //Store translations
+var currentLanguage = "EN"; //Default Language
+
+function changeLanguage(newLanguage) {
+  var currentLanguageElement = document.getElementById('current-language');
+  currentLanguageElement.textContent = newLanguage;
+  console.log("language switched");
+  loadTranslations(newLanguage).then(function () {
+    console.log("loading translations");
+    updatePageContent(newLanguage);
+  });
+  showLanguageOptions();
+}
+
+function loadTranslations(language) {
+  console.log("trying to fetch JSON file");
+  return new Promise((resolve, reject) => {
+    fetch('translations.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log("JSON file loaded");
+      translations[language] = data[language];
+      updatePageContent(language);
+    })
+    .catch(error => console.error('An error occurred', error));
+  })
+}
+
+function updatePageContent(language) {
+  var elements = document.querySelectorAll('[data-translation]');
+  elements.forEach(function (element) {
+    var key = element.getAttribute('data-translation');
+    if (translations[currentLanguage] && translations[currentLanguage][key]) {
+      element.textContent = translations[language][key];
+    }
+  });
+}
+
+// Default translation
+loadTranslations(currentLanguage).then(function () {
+  updatePageContent(currentLanguage);
+});
